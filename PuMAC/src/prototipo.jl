5376e28ec024e12912b7c919735828f96ce53bbd
JacoboL <43689134+JacoboL@users.noreply.github.com>
2021-01-27 16:27:38 -0600

    Update prototipo.jl

--- PuMAC/src/prototipo.jl
+++ PuMAC/src/prototipo.jl
@@ -5,19 +5,35 @@ function recolector(archivos::Array{String,undef}, columnas::Array{String,undef}
     #Extraer la informacion de los archivos
     count = 1
     arreglo::Array{undef,undef}
+    tamanio = 0
 
     for i in archivos
         arreglo[count] = DataFrame(CSV.File(i))
         count = count + 1
+        if tamanio < size(arreglo[count],1)
+            tamanio = size(arreglo[count],1)
+        end
     end
 
-    #Crear otro dataframe pero este contendra solo las columnas requeridas
-    df = DataFrame()
-    for i in 1:count, for j in 1:length(columnas)
-        #ESTO NO FUNCIONA
-        #if names(arreglo[i]) == columnas[j]
-        #    push!(df, COLUMNA)
-        #end
+#quitar columnas no requeridas de los dataframes
+
+
+#count = int cantidad de archivos
+#arreglo[] = Dataframes que contienen los archivos
+
+    for i in 1:count, for j in names(arreglo[count])
+        #quitar columnas no requeridas, los nombres
+        #de las columnas estan en un arreglo
+        #llamado "columnas" de tipo string
+    end
+
+
+    for i in 1:count#, for j in 1:tamanio
+        if nrow(arreglo[i]) < tamanio
+#crar un dataframe con numero de filas del tamaño de nrow(arreglo[i])-tamanio
+# e insertarlo abajo de arreglo[i]
+            arreglo[count] =
+        end
     end
 
     #Crear un archivo csv con el dataframe
