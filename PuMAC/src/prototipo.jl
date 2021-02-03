c8db5789f7d27672a73081006520bdf803a69ef0
JacoboL <43689134+JacoboL@users.noreply.github.com>
2021-02-03 13:16:43 -0600

    Update prototipo.jl

--- PuMAC/src/prototipo.jl
+++ PuMAC/src/prototipo.jl
@@ -1,62 +1,107 @@
 using CSV
 using DataFrames
 using JLD
+using FileIO
 
-function con_archivos(archivos::Array{String,undef}, columnas::Array{String,undef}, nombre_archivo::String, ruta::String = homedir(), exportto::String = "csv", faltantes::Bool = true)
+columnas = ["po","pob_f", "pob_m", "contr_nal", "nom_ent", "INEGI. Encuesta Intercensal 2015", "Va_cte", "ivf_itaee"]
 
-    count = 0
+function concatenador(archivos::Array, columnas::Array, nombre_archivo::String = "new_PuMAC.csv", faltantes::Bool = true)
+
+    # Asegurando que los valores introducidos sean los esperados
+    if(size(archivos,1) < 1)
+        error("Pon archivos csv para extraer, debe de ser un arreglo de tipo String")
+    end
+    
+    for i in archivos
+        if(contains(i, ".csv"))
+            error("Algun archivo no es csv o esta mal escrito, asegurarse que sea de la forma [\"Archivo_1.csv\", \"Archivo_2.csv\", ...]")
+        elseif(contains(i, ".jld"))
+            error("Algun archivo no es jld o esta mal escrito, asegurarse que sea de la forma [\"Archivo_1.jld\", \"Archivo_2.jld\", ...]")
+        end
+    end
+    
+    if(size(columnas,1) < 1)
+        error("Pon las columnas que quieres extraer, debe de ser un arreglo de tipo String")
+    end
+        
+    if(!contains(nombre_archivo, ".jld"))
+        error("El String que contiene al archivo necesita tener el tipo de archivo especificado o tipo de archivo no soportado")
+    end
+
+    # Se declara un arreglo con la misma cantidad de espacio que la cantidad de archivos pasados por el usuario
     arreglo = Array{DataFrame}(undef,size(archivos,1))
     tamanio = 0
-    nuevo_dataframe = DataFrame()
+    #count = 0
 
-    for i in archivos
-        arreglo[count] = DataFrame(CSV.File(i))
-        aux_arr[count] = DataFrame(ПУМАК = 1:nrow(arreglo[count]))
-        if tamanio < size(arreglo[count],1)
-            tamanio = size(arreglo[count],1)
-        end
-        count = count + 1
-    end
-
-    for i in 1:size(archivos,1), j in names(arreglo[i]), k in columnas
-        if(j==k)
-            aux_arr[i] = hcat(aux_arr[i],select(arreglo[i], Symbol(j)),makeunique=true)
-        end
-    end
-        
-    for i in 1:size(aux_arr,1)
-        select!(aux_arr[i], Not("ПУМАК"))
-    end
-
-
+    # Se extraen todos los archivos csv como DataFrames y se guardan en el arreglo,
+    # tambien se obtiene la dimension del DataFrame mas grande
+    cd("C:\\Users\\pillo\\OneDrive\\Escritorio\\Archivos")
     for i in 1:size(archivos,1)
-        if nrow(arreglo[i]) < tamanio
-            missing_dataframe = similar(arreglo[i], tamanio-nrow(arreglo[i]))
-            #nuevos valores a missing
-            append!(arreglo[i], missing_dataframe)
+        arreglo[i] = DataFrame(CSV.File(archivos[i]))
+        if tamanio < size(arreglo[i],1)
+            tamanio = size(arreglo[i],1)
         end
+        #=for j in names(arreglo[i])
+            if(k == j)
+                count = count +1
+            end
+        end=#
     end
     
-    for i in 1:size(archivos,1)
-       nuevo_dataframe = hcat(nuevo_dataframe, arreglo[i], makeunique=true)
+    #if(count == 0)
+    #    println("WARNING: una columna deseada no esta en los Archivos")
+    #end
+
+    # Se declara un Dataframe con la misma cardinalidad del DataFrame mas grande
+    df_nuevo = DataFrame(ПУМАК = 1:tamanio)
+
+    for i in 1:size(archivos,1), j in 1:size(names(arreglo[i]),1), k in columnas
+        # Se obtienen solo las columnas deseadas por el usuario
+        if (k == names(arreglo[i])[j])
+            # Se asigna a x con los calores de la columna deseada por el usuario
+            x = arreglo[i][:,k]
+            # Se convierte a x en tipo Any
+            x = convert(Array{Any,1}, x)
+
+            # Se normalizan filas de DataFrames con cardinalidad menor que la mas grande,
+            # si faltantes es true entonces los valores faltantes se normalizaran con el valor de missing, de otro modo se normalizara con ceros
+            if(nrow(arreglo[i]) < tamanio)
+                if (faltantes)
+                    append!(x, Array{Missing}(undef, tamanio-nrow(arreglo[i])))
+                else
+                    append!(x, zeros(tamanio-nrow(arreglo[i])))
+                end
+            end
+
+            # Se agrega la columna x al DataFrame
+            df_nuevo = hcat(df_nuevo, DataFrame(Symbol(k) => x), makeunique=true)
+        end
     end
 
-    if(lowercase(exportto) == "csv")
-        
-        CSV.write(nombre_archivo ,nuevo_dataframe)
-            
-        println("el archivo se guardo en", joinpath(homedir(), nombre_ruta))
-    else  if(lowercase(exportto) == "jld")
-        #exportar a jld
-    else
-        error("no se puede exportar al tipo de archivo $exportto")
+    # Se retira la columna auxiliar "ПУМАК" del DataFrame
+    select!(df_nuevo, Not("ПУМАК"))
+
+    # Se exporta al tipo de archivo deseado
+    if(contains(nombre_archivo, ".csv"))
+        CSV.write(nombre_archivo , df_nuevo)
+        print("el archivo se guardo en: ")
+        println( joinpath( pwd(), nombre_archivo))
+    elseif(contains(nombre_archivo, ".jld"))
+        save(joinpath(pwd(), nombre_archivo), "df", df_nuevo)
+        print("el archivo se guardo en: ")
+        println( joinpath( pwd(), nombre_archivo))
     end
-      
-    
-    return nevo_dataframe
+
+    return df_nuevo
 end
 
-function con_carpeta(carpeta::String, columnas::Array{String,undef}, nombre_archivo::String, exportto::String = "csv", faltantes::Bool = true)
+
+function concatenador(carpeta::String, columnas::Array, nombre_archivo::String = "new_PuMAC.csv", faltantes::Bool = true)
+    # Se extraen los archivos de tipo csv de la carpeta
     cd(carpeta)
     archivos = filter(x->endswith(x, ".csv"), readdir())
+
+    concatenador(archivos, columnas, nombre_archivo, faltantes)
 end
+
+Z = concatenador("C:\\Users\\pillo\\OneDrive\\Escritorio\\Archivos", columnas, "PrimeraPrueba.csv")
