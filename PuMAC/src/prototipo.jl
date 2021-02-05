29a2fd61f8f1231644e1f24ce6ab068d87eaa7f8
JacoboL <43689134+JacoboL@users.noreply.github.com>
2021-02-04 22:24:34 -0600

    Update prototipo.jl

--- PuMAC/src/prototipo.jl
+++ PuMAC/src/prototipo.jl
@@ -5,7 +5,7 @@ using FileIO
 
 columnas = ["po","pob_f", "pob_m", "contr_nal", "nom_ent", "INEGI. Encuesta Intercensal 2015", "Va_cte", "ivf_itaee"]
 
-function concatenador(archivos::Array, columnas::Array, nombre_archivo::String = "new_PuMAC.csv", faltantes::Bool = true)
+function concatenador(archivos::Array, columnas::Array, nombre_archivo::String = "new_PuMAC.csv", faltantes::Bool = true, filas)
 
     # Asegurando que los valores introducidos sean los esperados
     if(size(archivos,1) < 1)
@@ -13,19 +13,15 @@ function concatenador(archivos::Array, columnas::Array, nombre_archivo::String =
     end
     
     for i in archivos
-        if(contains(i, ".csv"))
+        if(!contains(i, ".csv"))
             error("Algun archivo no es csv o esta mal escrito, asegurarse que sea de la forma [\"Archivo_1.csv\", \"Archivo_2.csv\", ...]")
-        elseif(contains(i, ".jld"))
+        elseif(!contains(i, ".jld"))
             error("Algun archivo no es jld o esta mal escrito, asegurarse que sea de la forma [\"Archivo_1.jld\", \"Archivo_2.jld\", ...]")
         end
     end
     
     if(size(columnas,1) < 1)
         error("Pon las columnas que quieres extraer, debe de ser un arreglo de tipo String")
-    end
-        
-    if(!contains(nombre_archivo, ".jld"))
-        error("El String que contiene al archivo necesita tener el tipo de archivo especificado o tipo de archivo no soportado")
     end
 
     # Se declara un arreglo con la misma cantidad de espacio que la cantidad de archivos pasados por el usuario
@@ -104,4 +100,85 @@ function concatenador(carpeta::String, columnas::Array, nombre_archivo::String =
     concatenador(archivos, columnas, nombre_archivo, faltantes)
 end
 
+########################################################################
+
+function concatenador(archivos::Array, columnas::Dict, filas::Dict, nombre_archivo::String = "new_PuMAC.csv", faltantes::Bool = true)
+
+    # Asegurando que los valores introducidos sean los esperados
+    if(size(archivos,1) < 1)
+        error("Pon archivos csv para extraer, debe de ser un arreglo de tipo String")
+    end
+    
+    for i in archivos
+        if(!contains(i, ".csv"))
+            error("Algun archivo no es csv o esta mal escrito, asegurarse que sea de la forma [\"Archivo_1.csv\", \"Archivo_2.csv\", ...]")
+        elseif(!contains(i, ".jld"))
+            error("Algun archivo no es jld o esta mal escrito, asegurarse que sea de la forma [\"Archivo_1.jld\", \"Archivo_2.jld\", ...]")
+        end
+    end
+
+    # Se declara un arreglo con la misma cantidad de espacio que la cantidad de archivos pasados por el usuario
+    arreglo = Array{DataFrame}(undef,size(archivos,1))
+    tamanio = 0
+    #count = 0
+
+    # Se extraen todos los archivos csv como DataFrames y se guardan en el arreglo,
+    # tambien se obtiene la dimension del DataFrame mas grande
+    cd("C:\\Users\\pillo\\OneDrive\\Escritorio\\Archivos")
+    for i in 1:size(archivos,1)
+        arreglo[i] = DataFrame(CSV.File(archivos[i]))
+        if tamanio < size(arreglo[i],1)
+            tamanio = size(arreglo[i],1)
+        end
+    end
+    
+    # Se declara un Dataframe con la misma cardinalidad del DataFrame mas grande
+    df_nuevo = DataFrame(ПУМАК = 1:tamanio)
+
+    for i in 1:size(archivos,1), j in 1:size(names(arreglo[i]),1), k in get(x, "A", 1)
+        # Se obtienen solo las columnas deseadas por el usuario
+        if (k == names(arreglo[i])[j])
+            # Se asigna a x con los calores de la columna deseada por el usuario
+            x = arreglo[i][:,k]
+            # Se convierte a x en tipo Any
+            x = convert(Array{Any,1}, x)
+            
+            for m in collect(keys(filas))
+                if(k == m)
+                    deleteat!(x, get(filas, m, 1)) 
+                end
+            end
+
+            # Se normalizan filas de DataFrames con cardinalidad menor que la mas grande,
+            # si faltantes es true entonces los valores faltantes se normalizaran con el valor de missing, de otro modo se normalizara con ceros
+            if(size(x, 1) < tamanio)
+                if (faltantes)
+                    append!(x, Array{Missing}(undef, tamanio-nrow(arreglo[i])))
+                else
+                    append!(x, zeros(tamanio-nrow(arreglo[i])))
+                end
+            end
+
+            # Se agrega la columna x al DataFrame
+            df_nuevo = hcat(df_nuevo, DataFrame(Symbol(k) => x), makeunique=true)
+        end
+    end
+
+    # Se retira la columna auxiliar "ПУМАК" del DataFrame
+    select!(df_nuevo, Not("ПУМАК"))
+
+    # Se exporta al tipo de archivo deseado
+    if(contains(nombre_archivo, ".csv"))
+        CSV.write(nombre_archivo , df_nuevo)
+        print("el archivo se guardo en: ")
+        println( joinpath( pwd(), nombre_archivo))
+    elseif(contains(nombre_archivo, ".jld"))
+        save(joinpath(pwd(), nombre_archivo), "df", df_nuevo)
+        print("el archivo se guardo en: ")
+        println( joinpath( pwd(), nombre_archivo))
+    end
+
+    return df_nuevo
+end
+    
 Z = concatenador("C:\\Users\\pillo\\OneDrive\\Escritorio\\Archivos", columnas, "PrimeraPrueba.csv")
